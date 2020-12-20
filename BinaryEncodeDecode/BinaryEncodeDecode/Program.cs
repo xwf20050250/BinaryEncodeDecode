f5ffc6f6f15a47314b66ded0e932bbcabc7a45b0
c_iron <c_iron@126.com>
2020-12-20 14:52:02 +0800

    update

--- BinaryEncodeDecode/BinaryEncodeDecode/Program.cs
+++ BinaryEncodeDecode/BinaryEncodeDecode/Program.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using EDCodeCore;
+using System.Text;
+using System.IO;
 
 namespace BinaryEncodeDecode
 {
@@ -11,6 +13,17 @@ namespace BinaryEncodeDecode
     {
         static void Main(string[] args)
         {
+            //*
+            byte[] cfbyts = File.ReadAllBytes(@"C:\Users\c_iro\Desktop\cf.txt");
+            EDcodeHelper.Encode(cfbyts);
+            File.WriteAllBytes(@"C:\Users\c_iro\Desktop\cf_ed.txt", cfbyts);
+            //*/
+            /*
+            byte[] cfbyts = File.ReadAllBytes(@"C:\Users\c_iro\Desktop\cf_ed.txt");
+            EDcodeHelper.Decode(cfbyts);
+            File.WriteAllBytes(@"C:\Users\c_iro\Desktop\cf_dd.txt", cfbyts);
+            //*/
+            Console.Read();
         }
     }
 }
--- BinaryEncodeDecode/EDCodeCore/EDcodeHelper.cs
+++ BinaryEncodeDecode/EDCodeCore/EDcodeHelper.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace EDCodeCore
 {
@@ -21,19 +22,56 @@ namespace EDCodeCore
             return (byte)(inbyte ^ s_factor);
         }
 
+        public static void Encode(byte[] inoutbytes)
+        {
+            for (int i = 0; i < inoutbytes.Length; i++)
+            {
+                inoutbytes[i] = (byte)(0xff & ((inoutbytes[i] >> 6) | (inoutbytes[i] << 2)));
+                inoutbytes[i] ^= s_factor;
+            }
+        }
+
+        public static string Encode(string in_path)
+        {
+            string out_path = "";
+            if (File.Exists(in_path))
+            {
+                throw new Exception($"file not exists: {in_path}");
+            }
+            out_path = $"{in_path}_encode";
+            byte[] fbyts = File.ReadAllBytes(in_path);
+            Encode(fbyts);
+            File.WriteAllBytes(out_path, fbyts);
+            return out_path;
+        }
+
         public static byte Decode(byte inbyte)
         {
             inbyte ^= s_factor;
             return (byte)(0xff & ((inbyte << 6) | (inbyte >> 2)));
         }
 
-        public static void Decode(byte[] inoutbyte)
+        public static void Decode(byte[] inoutbytes)
         {
-            for (int i = 0; i < inoutbyte.Length; ++i)
+            for (int i = 0; i < inoutbytes.Length; ++i)
             {
-                inoutbyte[i] ^= s_factor;
-                inoutbyte[i] = (byte)(0xff & ((inoutbyte[i] << 6) | (inoutbyte[i] >> 2)));
+                inoutbytes[i] ^= s_factor;
+                inoutbytes[i] = (byte)(0xff & ((inoutbytes[i] << 6) | (inoutbytes[i] >> 2)));
             }
+        }
+
+        public static string Decode(string in_path)
+        {
+            string out_path = "";
+            if (File.Exists(in_path))
+            {
+                throw new Exception($"file not exists: {in_path}");
+            }
+            out_path = $"{in_path}_decode";
+            byte[] fbyts = File.ReadAllBytes(in_path);
+            Decode(fbyts);
+            File.WriteAllBytes(out_path, fbyts);
+            return out_path;
         }
     }
 }
