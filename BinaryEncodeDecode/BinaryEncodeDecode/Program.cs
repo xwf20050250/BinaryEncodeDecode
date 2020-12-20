db0228d604de06117efd4234289261e3f12218a6
c_iron <c_iron@126.com>
2020-12-20 17:12:58 +0800

    update

--- BinaryEncodeDecode/BinaryEncodeDecode/Program.cs
+++ BinaryEncodeDecode/BinaryEncodeDecode/Program.cs
@@ -6,24 +6,56 @@ using System.Threading.Tasks;
 using EDCodeCore;
 using System.Text;
 using System.IO;
+using CommandLine;
 
 namespace BinaryEncodeDecode
 {
     class Program
     {
+        public class Options
+        {
+            [Option('e', "encode", HelpText = "编码输入文件")]
+            public string EncodeFile { get; set; }
+            [Option('d', "decode", HelpText = "解码输入文件")]
+            public string DecodeFile { get; set; }
+        }
+
         static void Main(string[] args)
         {
-            //*
-            byte[] cfbyts = File.ReadAllBytes(@"C:\Users\c_iro\Desktop\cf.txt");
-            EDcodeHelper.Encode(cfbyts);
-            File.WriteAllBytes(@"C:\Users\c_iro\Desktop\cf_ed.txt", cfbyts);
-            //*/
-            /*
-            byte[] cfbyts = File.ReadAllBytes(@"C:\Users\c_iro\Desktop\cf_ed.txt");
-            EDcodeHelper.Decode(cfbyts);
-            File.WriteAllBytes(@"C:\Users\c_iro\Desktop\cf_dd.txt", cfbyts);
-            //*/
+            Parser.Default.ParseArguments<Options>(args).WithParsed(Run);
             Console.Read();
+        }
+
+        static void Run(Options options)
+        {
+            if (!string.IsNullOrEmpty(options.EncodeFile))
+            {
+                EncodeFile(options.EncodeFile);
+            }
+            if (!string.IsNullOrEmpty(options.DecodeFile))
+            {
+                DecodeFile(options.DecodeFile);
+            }
+        }
+
+        static void EncodeFile(string file_path)
+        {
+            if (!File.Exists(file_path))
+            {
+                return;
+            }
+            EDcodeHelper.Encode(file_path);
+            Console.WriteLine($"success to encode file: {file_path}");
+        }
+
+        static void DecodeFile(string file_path)
+        {
+            if (!File.Exists(file_path))
+            {
+                return;
+            }
+            EDcodeHelper.Decode(file_path);
+            Console.WriteLine($"success to decode file: {file_path}");
         }
     }
 }
